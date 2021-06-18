d313561bfad357dfbb1869b8e41f1f6634f7bc24
Derrick Mak <derrick.mak@derricks-mbp.lan>
2021-06-18 17:27:19 -0400

    uploaded image

--- about.php
+++ about.php
@@ -36,7 +36,7 @@
                 <h1>About Us</h1>
                 <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. See our shop below!</h2>
 
-                <img src="images/shop.png" alt="Chairs in barbershop" width="500" height="600">
+                <img src="images/shop.png">
                 <img src="images/timekitLogo.png">
             </div>
         </div>
